05ae0d2b375a5082d8aa21a868f4aec923ce6b32
flymax79 <flymax79@gmail.com>
2025-11-12 00:25:17 +0200

    Config: Add necessary Next.js and dependency files to fix 404

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // This enables Tailwind CSS during the Vercel build process.
+  // We don't need experimental features for this app.
+};
+
+module.exports = nextConfig;
